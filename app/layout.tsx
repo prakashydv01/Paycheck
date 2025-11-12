c56e99bc887ae00000df406d4d6828b5202a3221
prakashydv01 <135949364+prakashydv01@users.noreply.github.com>
2025-11-12 18:38:35 +0545

    adsense file added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Header from '@/app/components/Header'
 import Footer from './components/Footer'
+import Script from 'next/script'
 
 export const metadata = {
   title: "Paycheck Calculator | Federal & State Tax Estimator",
@@ -11,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5019867963652774"
+     ></script>
+      </head>
       <body className="bg-white text-gray-900 transition-colors duration-300">
         <Header />
         {children}
