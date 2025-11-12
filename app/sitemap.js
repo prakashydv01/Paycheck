3f32a63ebafa978f826e7a6d4fac0d889a7530ef
prakashydv01 <135949364+prakashydv01@users.noreply.github.com>
2025-11-12 20:31:18 +0545

    site & robot file added

---
+++ app/sitemap.js
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    { url: "https://uspaycheck.com/", lastModified: new Date() },
+    
+    { url: "https://uspaycheck.com/about", lastModified: new Date() },
+    { url: "https://uspaycheck.com/contact", lastModified: new Date() },
+  ];
+}
